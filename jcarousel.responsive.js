91805b716481218f4135360f2e304186db3575f8
frederikDarling <56248424+frederikDarling@users.noreply.github.com>
2022-06-22 09:05:24 +0200

    Revert "comments"
    This reverts commit c07ec5817b07fc29bc2c8a63d2a05f3f2ad493ce.

--- jcarousel.responsive.js
+++ jcarousel.responsive.js
@@ -1,36 +1,29 @@
 (function($) {
     $(function() {
-        // Selects the jCarousel
         var jcarousel = $('.jcarousel');
 
         jcarousel
             .on('jcarousel:reload jcarousel:create', function () {
-                // The inner width is selected and saved in the width variable
                 var carousel = $(this),
                     width = carousel.innerWidth();
 
-                // The width is decided
                 if (width >= 600) {
                     width = width / 3;
                 } else if (width >= 350) {
                     width = width / 2;
                 }
 
-                // The width is set of the carousel
                 carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
             })
             .jcarousel({
-                // Wrap is set to be circular
                 wrap: 'circular'
             });
 
-            // Prevoius button is selected and target i decreased by one 
         $('.jcarousel-control-prev')
             .jcarouselControl({
                 target: '-=1'
             });
 
-            // Next button is selected and target i increased by one 
         $('.jcarousel-control-next')
             .jcarouselControl({
                 target: '+=1'
